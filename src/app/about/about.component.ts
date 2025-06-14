4ce2d103494b2f42e6896c27865991164e03b730
amr <amrashraf314@gmail.com>
2025-06-14 03:03:45 +0300

    first commit for first angular portfolio project

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './about.component.html', // Path file html
+  styleUrl: './about.component.scss' // path of styling 
+})
+export class AboutComponent {
+  color = "Red";
+  isDisabled:boolean = false
+  imgSrc:string = "../../favicosn.ico";
+  val:string = "test";
+  altText = "ASfassafas";
+
+print(){
+  console.log("Print");
+  
+}
+
+
+updateColor(){
+  
+  if(this.color ==="Blue") this.color = "Navy";
+  else this.color = "Blue";
+}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { AboutComponent } from './about/about.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeroSectionComponent,AboutComponent,SkillsComponent,PortfolioComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
